6330c01d4c55159e76bfc3acb4dff00114c1d059
Paige Huffman <paigehuffman@Paiges-Air-3.cable.rcn.com>
2021-09-06 18:27:15 -0400

    add arrays and skeleton for code

--- assets/script.js
+++ assets/script.js
@@ -10,5 +10,44 @@ function writePassword() {
 
 }
 
+var generatePassword = function() {
+  let passwordObj = {
+    password: "",
+    length: passwordLength(),
+    uppercase: false,
+    lowercase: false,
+    special: false,
+    numeric: false
+  }
+
+  console.log(passwordObj.length);
+  passwordObj.uppercase = window.confirm("Do you want to include uppercase letters?");
+  passwordObj.lowercase = window.confirm("Do you want to include lowercase letters?");
+  passwordObj.special = window.confirm("Do you want to include special characters?");
+  passwordObj.numeric = window.confirm("Do you want to include numbers?");
+
+  
+
+
+  return newPassword;
+}
+
+var passwordLength = function() {
+  let desiredLength = 0;
+  while (desiredLength < 8 || desiredLength > 128) {
+    desiredLength = window.prompt("How many characters do you need your password to be?");
+    desiredLength = parseInt(desiredLength);
+    if (desiredLength < 8 || desiredLength > 128) {
+      window.alert("Please enter a password length between 8 and 128.");
+    }
+  }
+  return desiredLength;
+}
+
+const uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const symbols = ["!", "@", "#", "$", "%", "^", "&","*", "(", ")", "-", "+", "=", ":", "<", ">", "?"]
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
